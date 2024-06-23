f5b891e8466e033de233bc579a19a886ae6b8e70
DimensionalScoop <m@pernklau.eu>
2024-06-23 10:16:19 +0200

    reflect changes to the telegram API

--- bot.py
+++ bot.py
@@ -1,8 +1,6 @@
-import telegram.ext as tele
-from telegram.ext import CommandHandler
-import telegram
+from telegram import Bot
+from telegram.ext import CommandHandler, ContextTypes, ApplicationBuilder
 import logging
-import time
 
 import parser
 import fun
@@ -14,36 +12,29 @@ logging.basicConfig(
 token_file = "token"
 with open(token_file) as f:
     token = f.readline().replace("\n", "")
-updater = tele.Updater(token=token, use_context=True)
-dispatcher = updater.dispatcher
+app = ApplicationBuilder().token(token).build()
+
+async def start(update, context):
+    await help(update, context)
 
 
-def start(update, context):
-    context.bot.send_message(
-        chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!"
-    )
+async def hey(update, context):
+    await update.message.reply_text("Heyya!!")
 
 
-def hey(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Heyya!!")
-
-
-def roll_dice(update, context):
-    chat_id = update.effective_chat.id
+async def roll_dice(update, context):
     args = " ".join(context.args)
     answer = parser.handle_dice_command(args)
-    context.bot.send_message(chat_id=chat_id, text=answer,parse_mode="MARKDOWN")
+    await update.message.reply_markdown(answer)
 
 
-def roll_single_die(update, context):
-    chat_id = update.effective_chat.id
+async def roll_single_die(update, context):
     args = "d"+" ".join(context.args)
     answer = parser.handle_dice_command(args)
-    context.bot.send_message(chat_id=chat_id, text=answer,parse_mode="MARKDOWN")
+    await update.message.reply_markdown(answer)
 
 
-def roll_fate(update, context):
-    chat_id = update.effective_chat.id
+async def roll_fate(update, context):
     args = " ".join(context.args)
 
     complete_args = "dF "
@@ -56,19 +47,16 @@ def roll_fate(update, context):
     except ValueError:
         complete_args += args
     answer = parser.handle_dice_command(complete_args)
-    context.bot.send_message(chat_id=chat_id, text=answer,parse_mode="MARKDOWN")
+    await update.message.reply_markdown(answer)
 
 
-def roll_dsa(update, context):
-    chat_id = update.effective_chat.id
+async def roll_dsa(update, context):
     args = " ".join(context.args)
     answer = parser.handle_dice_command("dsa " + args)
-    context.bot.send_message(chat_id=chat_id, text=answer,parse_mode="MARKDOWN")
+    await update.message.reply_markdown(answer)
 
 
-def help(update, context):
-    chat_id = update.effective_chat.id
-
+async def help(update, context):
     answer = """
 A dice rolling bot by @elayn
 ------------------------------------
@@ -89,24 +77,21 @@ roll a Fate check
 E.g.: `/fate +2` rolls four fudge dice and adds two
     """
 
-    context.bot.send_message(chat_id=chat_id, text=answer,parse_mode="MARKDOWN")
+    await update.message.reply_markdown(answer)
 
 
 start_handler = CommandHandler("start", start)
-dispatcher.add_handler(start_handler)
-dispatcher.add_handler(CommandHandler("hey", hey))
-dispatcher.add_handler(CommandHandler("roll", roll_dice))
-dispatcher.add_handler(CommandHandler("r", roll_dice))
-dispatcher.add_handler(CommandHandler("d", roll_single_die))
-dispatcher.add_handler(CommandHandler("dsa", roll_dsa))
-dispatcher.add_handler(CommandHandler("fate", roll_fate))
-dispatcher.add_handler(CommandHandler("dF", roll_fate))
-dispatcher.add_handler(CommandHandler("df", roll_fate))
-dispatcher.add_handler(CommandHandler("help", help))
-dispatcher.add_handler(CommandHandler("give_me_the_math", fun.give_me_the_math))
+app.add_handler(start_handler)
+app.add_handler(CommandHandler("hey", hey))
+app.add_handler(CommandHandler("roll", roll_dice))
+app.add_handler(CommandHandler("r", roll_dice))
+app.add_handler(CommandHandler("d", roll_single_die))
+app.add_handler(CommandHandler("dsa", roll_dsa))
+app.add_handler(CommandHandler("fate", roll_fate))
+app.add_handler(CommandHandler("dF", roll_fate))
+app.add_handler(CommandHandler("df", roll_fate))
+app.add_handler(CommandHandler("help", help))
+app.add_handler(CommandHandler("give_me_the_math", fun.give_me_the_math))
 
-updater.start_polling()
+app.run_polling()
 print("Started.")
-
-while True:
-    time.sleep(1)
